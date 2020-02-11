45f9a421c2b63723911eb3d5119261a1b5179210
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-02-11 15:33:54 +0600

    working  on  edit  button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { ListEmployeesComponent } from './employee/list-employees.component';
 const appRoutes: Routes = [
   { path: 'list', component: ListEmployeesComponent },
   { path: 'create', component: CreateEmployeeComponent },
+  { path: 'edit/:id', component: CreateEmployeeComponent },
+
   { path: '', redirectTo: '/list', pathMatch: 'full' }
 ];
 
