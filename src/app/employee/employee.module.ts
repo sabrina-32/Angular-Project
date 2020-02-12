b5647f5f07206793912080a7170fa892a80460a2
Sabrina Nusrat Ayvi <sabrina.ayvi@selise.com.bd>
2020-02-12 13:19:43 +0600

    feature module added

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -9,12 +9,14 @@ import{EmployeeRoutingModule} from './employee-routing.module';
 import { CreateEmployeeComponent } from './create-employee.component';
 import { ListEmployeesComponent } from './list-employees.component';
 import { from } from 'rxjs';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    SharedModule
   ],
   declarations: [
     CreateEmployeeComponent,
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  imports:[
+
+  ],
+  declarations: [],
+  exports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
